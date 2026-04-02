fdaed93527d7b02ec47e3a6e689854cd44dddc73
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-02 14:16:56 +0200

    creazione switch in js

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const img_lampadina = document.querySelector('.lampada');
+const btn_lapmada = document.querySelector('.btn-lampada');
+
+function switch_lampada() {
+    if (btn_lapmada.innerHTML.toLowerCase() === 'accendi') {
+        img_lampadina.src = "./img/yellow_lamp.png";
+        btn_lapmada.innerHTML  = 'Spegni';
+    }else{
+        img_lampadina.src = "./img/white_lamp.png";
+        btn_lapmada.innerHTML  = 'Accendi';
+    }
+}
+
+btn_lapmada.addEventListener('click', switch_lampada);
